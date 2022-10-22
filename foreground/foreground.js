43439d08f6b07745a20b6f310a9ea81e8743e5dc
mk <kaushikmony17@gmail.com>
2022-10-22 20:25:04 +1100

    [WIP] Fresh  start deleted everything

--- foreground/foreground.js
+++
@@ -1,9 +0,0 @@
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        const {action, ...data} = request;
-        handleRequest(action, data, sendResponse)
-    }
-);
-
-
